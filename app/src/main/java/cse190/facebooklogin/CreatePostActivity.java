9fea1066e94ac9ed4149d8e45105e30c06d9a896
ajabasa <amjabasa@gmail.com>
2015-11-25 17:33:37 -0800

    minor changes, tried creating JSON object

--- app/src/main/java/cse190/facebooklogin/CreatePostActivity.java
+++ app/src/main/java/cse190/facebooklogin/CreatePostActivity.java
@@ -1,7 +1,18 @@
 package cse190.facebooklogin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /******************************************************************************
  * Activity created/ called from a successful FB Login
@@ -11,6 +22,13 @@ public class CreatePostActivity extends Activity {
      * Constant String for Log Messages
      *************************************************************************/
     private static final String TAG = "CreatePostActivity";
+
+    JSONObject info = new JSONObject();
+    /*String user_id;
+    String name;
+    String gender;
+    String profilepic_url;*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +71,28 @@ public class CreatePostActivity extends Activity {
          * first_name, last_name, etc. I then output the message into the
          * the Log.
          *********************************************************************/
-        /*
+
+
+
 
         GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(),
                 new GraphRequest.GraphJSONObjectCallback() {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
                         Log.e(TAG, "user_me request = " + object.toString());
+
+                        //json stuff
+                        try {
+                            //put fields into our Json info object
+                            info.put("user_id", object.getString("id"));
+                            info.put("name", object.getString("name"));
+                            info.put("gender", object.getString("gender"));
+                            info.put("profilepic_url", object.getString("link"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
                     }
                 });
         Bundle parameters = new Bundle();
@@ -67,6 +100,76 @@ public class CreatePostActivity extends Activity {
         request.setParameters(parameters);
         request.executeAsync();
 
-        */
+        //volley stuff
+        /*String url = "http://httpbin.org/post";
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response).getJSONObject("form");
+                            String site = jsonResponse.getString("site"),
+                                    network = jsonResponse.getString("network");
+                            System.out.println("Site: "+site+"\nNetwork: "+network);
+                            System.out.println("Volley object: " + jsonResponse.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<>();
+                // the POST parameters:
+                params.put("site", "code");
+                params.put("network", "tutsplus");
+                return params;
+            }
+        };
+        Volley.newRequestQueue(getApplicationContext()).add(postRequest);*/
+
+
+    //all this is in onCreate... not sure if it should be
+
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendPost(View view) {
+        Intent intent = new Intent(this, CrimeListActivity.class);
+
+        EditText editText1 = (EditText) findViewById(R.id.time);
+        String message1 = editText1.getText().toString();
+        try {
+            info.put("time", message1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        EditText editText2 = (EditText) findViewById(R.id.date);
+        String message2 = editText2.getText().toString();
+        try {
+            info.put("date", message2);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        EditText editText3 = (EditText) findViewById(R.id.location);
+        String message3 = editText3.getText().toString();
+        try {
+            info.put("location", message3);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(intent);
     }
 }
